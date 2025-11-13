e0023f78b69173377ecd4cad4919633e36196d16
Viacheslav Blazhkov <blazkov253@gmail.com>
2025-11-13 13:17:54 +0200

    [#62] Moved TG settings to env

--- helpers/telegram.js
+++ helpers/telegram.js
@@ -1,12 +1,9 @@
-const {post} = require("axios");
 const axios = require("axios");
-const TELEGRAM_BOT_TOKEN = '8273401211:AAF4LfnM9tlRpeAJPJjQgZQYYNedRYjYwlc';
-const TELEGRAM_CHAT_ID = '-1002530863997';
+
+const TELEGRAM_BOT_TOKEN = process.env.TELEGRAM_BOT_TOKEN;
+const TELEGRAM_CHAT_ID = process.env.TELEGRAM_CHAT_ID;
 
 async function sendTelegramMessage(message) {
-	//if (APP_ENV === 'local') {
-	//	return;
-	//}
 	try {
 		await axios.post(`https://api.telegram.org/bot${TELEGRAM_BOT_TOKEN}/sendMessage`, {
 			chat_id: TELEGRAM_CHAT_ID,
@@ -15,6 +12,6 @@ async function sendTelegramMessage(message) {
 	} catch (err) {
 		console.error('Помилка надсилання в Telegram:', err.message);
 	}
-};
+}
 
 module.exports = sendTelegramMessage;
