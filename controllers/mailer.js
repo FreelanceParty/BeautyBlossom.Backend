766bfde26f2dff1932a995ee08b765dc994468f8
Viacheslav Blazhkov <blazkov253@gmail.com>
2026-02-26 10:48:58 +0200

    [FIX]

--- controllers/mailer.js
+++ controllers/mailer.js
@@ -1,14 +1,15 @@
 const nodemailer = require("nodemailer");
 const sendTelegramMessage = require("../helpers/telegram");
-const {APP_ENV, EMAIL_HOST, EMAIL_PORT, EMAIL_USER} = process.env;
+const {APP_ENV, EMAIL_HOST, EMAIL_PORT, EMAIL_USER, EMAIL_PASS} = process.env;
 
 const transporter = nodemailer.createTransport({
 	host:   EMAIL_HOST,
-	port:   EMAIL_PORT,
-	secure: true,
+	port:   Number(EMAIL_PORT),
+	secure: false,
+	requireTLS: true,
 	auth:   {
 		user: EMAIL_USER,
-		pass: 'RuiaOnDF6RL9m9Gl',
+		pass: EMAIL_PASS,
 	},
 });
 
