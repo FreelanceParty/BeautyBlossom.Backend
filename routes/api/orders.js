d6ed578304ae8afea3e97c6cd7f5e0fd5f113954
Viacheslav Blazhkov <blazkov253@gmail.com>
2026-05-16 02:21:55 +0300

    [FIX] Fixed order status updating

--- routes/api/orders.js
+++ routes/api/orders.js
@@ -14,14 +14,14 @@ router.get("/", authenticate, ctrl.getAll);
 
 router.post("/", optionalAuthenticate, validateBody(schemas.addSchema), ctrl.add);
 
-router.get("/:id", authenticate, isValidId, ctrl.getById);
-
 router.put("/:id", authenticate, isValidId, validateBody(schemas.addSchema), ctrl.updateById);
 
 router.patch("/:id/checked", authenticate, isValidId, validateBody(schemas.updateCheckedSchema), ctrl.updateChecked);
 router.patch("/:id/status", authenticate, isValidId, validateBody(schemas.updateStatusSchema), ctrl.updateStatus);
 
 router.delete("/:id", authenticate, isValidId, ctrl.deleteById);
+
+router.get("/:id", authenticate, isValidId, ctrl.getById);
 
 module.exports = router;
  
